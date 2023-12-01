beecb5fa5c029f4d806085ce5614824ff8866d6e
ogolla23 <leonardogolla23@gmail.com>
2023-12-01 04:33:25 -0800

    task 3

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,25 +1,15 @@
 #!/usr/bin/python3
+"""A script that takes in a URL,sends a request to the URL
+and displays the body of the response (decoded in utf-8).
 """
-A script that displays the body of a Response
-"""
-from sys import argv
-from urllib.request import Request, urlopen
-from urllib.error import HTTPError
-
-
-def main(argv):
-    """
-    A function that manage urllib.error.HTTPError exceptions and
-    print: Error code: followed by the HTTP status code
-    """
-    url = argv[1]
-    req = Request(url)
-    try:
-        with urlopen(req)as response:
-            print(response.read().decode9"ascii"))
-    except HTTPError as  e:
-        print("Error code: {}".format(e.code))
 
 
 if __name__ == "__main__":
-    main(argv)
+    import sys
+    from urllib import request, error
+
+    try:
+        with request.urlopen(sys.argv[1]) as res:
+            print(res.read().decode('UTF-8'))
+    except error.HTTPError as er:
+        print('Error code:', er.code)
--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-"""A script that
-- takes in a URL
-- sends a request to the URL
-- displays the body of the response.
+"""A script that sends a request to the URL
+and displays the body of the response.
 """
 import sys
 import requests
@@ -11,8 +9,8 @@ import requests
 if __name__ == "__main__":
     url = sys.argv[1]
 
-    r = requests.get(url)
-    if r.status_code >= 400:
-        print("Error code: {}".format(r.status_code))
+    req = requests.get(url)
+    if req.status_code >= 400:
+        print("Error code: {}".format(req.status_code))
     else:
-        print(r.text)
+        print(req.text)
