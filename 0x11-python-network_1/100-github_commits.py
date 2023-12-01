b4bbfa8830ee747342f672c1ae593d758e907c1d
ogolla23 <leonardogolla23@gmail.com>
2023-12-01 04:51:38 -0800

    fix

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     url = "https://api.github.com/repos/{}/{}/commits".format(
         sys.argv[2], sys.argv[1])
 
-    req= requests.get(url)
+    req = requests.get(url)
     commits = req.json()
     try:
         for i in range(10):
--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     url = argv[1]
     value = {"email": argv[2]}
     data = urlencode(value).encode("ascii")
-    r= Request(url, data)
+    r = Request(url, data)
 
     with urlopen(r) as response:
         print(response.read().decode("utf-8", "replace"))
